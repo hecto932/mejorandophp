5d2be79e199bf6e9333b0158e01bd7fe7eedc6a6
Hector <hector@hector-M2420.(none)>
2014-05-29 00:45:16 -0430

    Refractorizando el frontend controller y URL amigables. Solo con PHP. Mini framework

--- index.php
+++ index.php
@@ -5,20 +5,8 @@
 	require "config.php";
 	require "helpers.php";
 
-	//LLAMAR A LOS CONTROLADORES INDIVIDUALES
-	
-	//index.php
-	if(empty($_GET['url'])){
-		require "controllers/home.php";
-	}
-	//index.php?url=contactos
-	elseif($_GET['url'] == 'contactos'){
-		require "controllers/contactos.php";
-	}
-	else
-	{
-		header("HTTP/1.0 404 Not Found");
-		exit("Pagina no encontrada");
-	}
+	//LLAMAR AL CONTROLADOR INDICADO
+	controller($_GET['url']);
+
 
 	
