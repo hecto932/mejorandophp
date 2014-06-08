21e862ecc4eae1828743190a0e408c822d24503d
hecto932 <hecto932@gmail.com>
2014-06-07 21:38:20 -0430

    Removiendo controladores viejos

--- controllers/home.php
+++
@@ -1,8 +0,0 @@
-<?php 
-		
-	$language = "PHP";
-	$confidencia = "wow such private very confidential";
-	$titulo = "MejorandoPHP";
-
-	//LLAMANDO UNA FUNCION
-	view('home', compact('language','titulo'));
